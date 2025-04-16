c15285353c163664d4434b01dd104d4f48354870
gitaher <1750816649@qq.com>
2025-04-16 20:53:37 +0800

    a level

---
+++ Source/MyGTA/MyGameModeBase_1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBase_1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGTA_API AMyGameModeBase_1 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MyGTA/little_gamemode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "little_gamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYGTA_API Alittle_gamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
