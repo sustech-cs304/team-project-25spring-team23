c9ea85b4e21bbff480a7a718c2f115ea04783251
solovesonxi <2172449563@qq.com>
2025-04-24 16:42:23 +0800

    add some lamp samples

---
+++ Source/MyGTA/RoadLightSpawner.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MYGTA_API RoadLightSpawner
+{
+public:
+	RoadLightSpawner();
+	~RoadLightSpawner();
+};
